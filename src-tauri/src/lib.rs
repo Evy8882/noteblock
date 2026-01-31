f04f6fee99f3eea3c027b0396841e5e7e9b3cc88
Evy8882 <evy1339@gmail.com>
2026-01-31 14:38:29 -0300

    feat: implement file creation functionality with JSON data handling

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,45 @@
-mod models;
+use serde::{Deserialize, Serialize};
+use serde_json::{from_str, to_string_pretty};
+use crate::models::files::File;
+use crate::models::fields::Field;
+use uuid::Uuid;
+
+fn get_json_data() -> Option<Vec<File>> {
+    let file_content = match std::fs::read_to_string("data/files.json") {
+        Ok(content) => content,
+        Err(_) => String::from("[]"),
+    };
+
+    let data = match from_str::<Vec<File>>(&file_content) {
+        Ok(data) => Some(data),
+        Err(_) => None,
+    };
+    data
+}
+
+fn save_json_data(data: &Vec<File>) {
+    let json_string = match to_string_pretty(data) {
+        Ok(json) => json,
+        Err(_) => return,
+    }
+    std::fs::write("data/files.json", json_string).expect("Unable to write file");
+}
 
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn create_file(file_name: &str) {
+    let mut current_files: Vec<File> = match get_json_data() {
+        Some(data) => data,
+        None => Vec::new(),
+    };
+
+    let id = Uuid::new_v4().as_simple().to_string();
+    let new_file = File {
+        id: id,
+        name: String::from(file_name),
+        fields: Vec::new(),
+    };
+    current_files.push(new_file);
+    save_json_data(&current_files);
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -10,7 +47,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
-            greet
+            create_file,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
